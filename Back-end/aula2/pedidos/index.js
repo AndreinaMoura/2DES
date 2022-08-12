e69ff5fa85c7992642085318addb04c50cc6d5b1
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-08-12 10:07:13 -0300

    projeto

--- Back-end/aula2/pedidos/index.js
+++ Back-end/aula2/pedidos/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
@@ -8,35 +9,38 @@ const con = mysql.createConnection({
     database: 'pedidos'
 });
 
-app.use(express.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/pedidos', (req, res) => {
-    res.json("Dados recebidos com sucesso, estamos enviando para o nosso Banco de Dados");
-    let priNome = req.query.priNome;
-    let sobrenome = req.query.sobrenome;
-    let endereco = req.query.endereco;
-    let telefones = req.query.telefones;
-    // update pedidos;
-    // where nome = req.params.priNome;
-    // set id = req.params.id;
+// app.use(express.json());
+
+app.post('/pedidos', (req, res) => {
+    var id = -1;
+    let priNome = req.body.priNome;
+    let sobrenome = req.body.sobrenome;
+    let endereco = req.body.endereco;
+    let telefones = req.body.telefones.split("\r\n");
     let string = `insert into clientes value(null,'${priNome}','${sobrenome}','${endereco}')`;
-    let string2 = `insert into telefones value(7,'${telefones}')`;
-    con.query(string2, (err, result) => {
-        if (err == null) {
-            res.json("Dados recebidos com sucesso e enviando para o nosso Banco de Dados");
-        } else {
-            res.json("Dados recebidos com sucesso, porém não conseguimos enviar para o Banco de Dados");
-        }
-    });
     con.query(string, (err, result) => {
         if (err == null) {
-            res.json("Dados recebidos com sucesso e enviando para o nosso Banco de Dados");
+            res.json("Dados recebidos com sucesso e enviados para o nosso Banco de Dados");
+            id = result.insertId;
+            telefones.forEach(e => {
+                string = `insert into telefones value('${id}','${e}')`;
+                con.query(string, (err, result) => {
+                    if (err == null) {
+                        res.json("Dados telefones com sucesso e enviados para o nosso Banco de Dados");
+                    }
+                });
+            });
         } else {
-            res.json("Dados recebidos com sucesso, porém não conseguimos enviar para o Banco de Dados");
+            res.json("Dados recebidos com sucesso, porém não conseguimos enviar para o banco de dados");
         }
     });
 });
 
-app.listen(3000, () => {//acima de 1000 ou o numero 80
+app.listen(3000, () => {
     console.log("Respondendo na porta 3000");
 });
+// con.query(sql, err sucesso);
