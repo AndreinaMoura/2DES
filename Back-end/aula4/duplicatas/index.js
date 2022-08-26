e2bf8e23d3bc3a3be9eac072a4d59621312e2a5a
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-08-26 11:20:30 -0300

    projeto

--- Back-end/aula4/duplicatas/index.js
+++ Back-end/aula4/duplicatas/index.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
     database: 'duplicatas'
 });
 
-app.get('/duplicatas',(req,res)=>{
+app.get('/duplicatas/clientes',(req,res)=>{
     let string = "select * from vw_clientes";
     con.query(string,(err,result)=>{
         if(err == null){
@@ -19,7 +19,7 @@ app.get('/duplicatas',(req,res)=>{
     });
 });
 
-app.get('/duplicatas',(req,res)=>{
+app.get('/duplicatas/duplicatas',(req,res)=>{
     let string = "select * from duplicatas";
     con.query(string,(err,result)=>{
         if(err == null){
