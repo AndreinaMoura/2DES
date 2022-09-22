1af67d641b5738ad854eae3008bf850171b24389
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2022-09-22 11:29:23 -0300

    projeto

--- INDIMO/aula04/restaurante/App.js
+++ INDIMO/aula04/restaurante/App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './src/pages/Home/index.js';
-import Segunda from './src/pages/Segunda/index.js';
+import Descricao from './src/pages/Descricao/index.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Segunda" component={Segunda}/>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Descricao" component={Descricao}/>
       </Stack.Navigator>
     </NavigationContainer>
